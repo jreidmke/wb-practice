bd6bf702c479debdb445f4d0e22c59e967cdc744
James Reid <jreidmke@gmail.com>
2021-02-25 11:50:04 -0600

    completed vowel swap funciton

---
+++ app.js
@@ -0,0 +1,54 @@
+//With 2 pointers
+
+//String Reverse
+//Reverse String
+
+var reverseString = function(s) {
+    let counter = 0;
+    let currLeft;
+    for(let i = s.length - 1; i >= s.length / 2; i--) { //<== key part. You must stop reversing half way through. Otherwise, you will "unreverse" the string. 
+        currLeft = s[counter];
+        s[counter] = s[i];
+        s[i] = currLeft;
+        counter++;
+    };
+    return s;
+};
+
+//Limiting yourself to one pointer
+
+var reverseString = function(s) {
+    let currLeft;
+    for(let i = s.length - 1; i >= s.length / 2; i--) {
+        currLeft = s[s.length - i - 1];
+        s[s.length - i - 1] = s[i];
+        s[i] = currLeft;
+    };
+    return s;
+};
+
+//REVERSE VOWELS IN A STRING VOWEL REVERSE
+
+function reverseVowels2(s) {
+    const vowels = "aeiouAEIOU";
+    let sArr = [...s];
+    let left = 0;
+    let right = s.length - 1;
+    while(left < right) {
+        //if they are both vowels, switch em
+        if(vowels.indexOf(sArr[left]) !== -1 && vowels.indexOf(sArr[right]) !== -1) {
+            [sArr[left], sArr[right]] = [sArr[right], sArr[left]]
+            ++left;
+            --right;
+        };
+        if(vowels.indexOf(sArr[right]) == -1) {
+            --right;
+        };
+        //if left is a vowel but right isn't, move the r pointer left
+        if(vowels.indexOf(sArr[left]) == -1) {
+            ++left;
+        }
+        //if right is a vowel but left isn't, move the l right
+    };
+    return sArr.join('');
+};
