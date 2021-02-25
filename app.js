36cf5f69cc22f1f3c1bb3b16bd031fc78b22e3ad
James Reid <jreidmke@gmail.com>
2021-02-25 15:38:41 -0600

    anagram and some others

--- app.js
+++ app.js
@@ -104,3 +104,90 @@ var reverseWords = function(s) {
     };
     return nString.trim()
 };
+
+//DETECT CAPITAL FIRST PASS
+
+var detectCapitalUse = function(s) {
+    if(s.split("").every(x => x.toUpperCase() === x)) return true;
+    if(s.split("").every(x => x.toLowerCase() === x)) return true
+    if(s[0].toUpperCase() === s[0] && s.slice(1).split("").every(x => x.toLowerCase() === x)) return true;
+    return false;
+};
+
+//NOW USE CHAR AT
+
+
+//CONTAINS DUPLICATE
+var containsDuplicate = function(nums) {
+    let obj = {};
+  for(let n of nums) {
+    if(n in obj) return true;
+    obj[n] = 1;
+  };
+  return false;
+};
+
+var containsDuplicate = function(nums) {
+    nums.sort();
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i]===nums[i+1]) return true;
+  };
+  return false
+};
+
+
+//BEST TIME TO BUY AND SELL STOCK
+
+function maxProfit(prices) {
+    let profit = 0;
+    let buyingPrice = prices[0];
+    for(let i = 1; i < prices.length; i++) {
+      if(prices[i] > buyingPrice) {
+        profit += prices[i] - buyingPrice;
+        buyingPrice = prices[i];
+      } else {
+        buyingPrice = prices[i];
+      }
+    }
+    return profit;
+  }
+  
+  maxProfit([1,2,3,4,5])
+
+  //PALINDROME
+
+  let left = 0;
+  let right = s.length - 1;
+  while(left < right) {
+    if(s[left] !== s[right]) return false; 
+      ++left;
+      --right;
+  };
+  return true;
+
+  //ANAGRAM
+
+  function validAngram(s, t) {
+    if(s.length !== t.length) return false;
+    function freq(str) {
+      let obj = {};
+      for(let char of str) {
+        if(obj[char] > 0) {
+          obj[char]++;
+        } else {
+          obj[char]=1;
+        };
+      };
+      return obj;
+    };
+      let sObj = freq(s);
+      let tObj = freq(t);
+      let letters = Object.keys(sObj);
+      for(let l of letters) {
+        if(sObj[l] !== tObj[l]) return false;
+      }
+    return true;
+  }
+  
+  
+  
